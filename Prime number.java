a247f954605a32688a5d29a5e2ed4edf217c54c1
Amit Patnaik <77204104+Amit-45@users.noreply.github.com>
2022-08-17 01:21:48 +0530

    Create Prime number.java

---
+++ Prime number.java
@@ -0,0 +1,34 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	   Scanner sc=new Scanner(System.in);
+	   int t=sc.nextInt();
+	   
+	   for (int i=0;i<t ;i++ )
+	   {
+	       int n=sc.nextInt();
+	       
+	       
+	       int count=0;
+	      for (int j=2;j*j<n ;j++ )
+	      {
+	          if(n%j==0) 
+	          {
+	              count++;
+	          }
+	      }
+	      if(count==0){System.out.println("prime");}
+	      else{System.out.println("not prime");}
+	       
+	   } 
+	   
+	}
+}
